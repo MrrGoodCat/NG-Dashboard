be28c11fd5c95b31ba72e6b667c8f3275cb1276a
MrrGoodCat <mrrgoodcat@gmail.com>
2018-12-28 01:50:36 +0200

    Sidebar menu without navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SidebarModule } from "./sidebar/sidebar.module";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SidebarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

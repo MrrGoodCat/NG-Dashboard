cb6b197e77e23215fad14c583c3f29a07eefe88d
MrrGoodCat <mrrgoodcat@gmail.com>
2019-01-04 01:52:07 +0200

    Angular material charts and bootstrap doughut chart

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,17 +5,20 @@ import { SharedModule } from "../shared/shared.module";
 import { LineChartComponent } from './line-chart.component';
 import { DashboardService } from './dashboard.service';
 import { HttpClientModule } from "@angular/common/http";
+import { DoughnutChartComponent } from './doughnut-chart.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   imports: [
     SharedModule,
     HttpClientModule,
+    MDBBootstrapModule,
     AppRoutingModule
   ],
   exports: [
     DashboardComponent
   ],
-  declarations: [DashboardComponent, LineChartComponent],
+  declarations: [DashboardComponent, LineChartComponent, DoughnutChartComponent],
   providers: [DashboardService]
 })
 export class DashboardModule { }
--- src/app/dashboard/line-chart.component.ts
+++ src/app/dashboard/line-chart.component.ts
@@ -10,7 +10,9 @@ import { DashboardService } from './dashboard.service';
 export class LineChartComponent implements OnInit {
 
   chart = [];
-
+  data = [{x: 1, y: 0}, {x: 6, y: 100}, {x: 9, y: 47}, {x: 15, y: 78}, {x: 20, y: 26}, {x: 24, y: 45}, {x: 31, y: 0}];
+  dataTwo = [{x: 1, y: 0}, {x: 9, y: 90}, {x: 13, y: 29}, {x: 17, y: 71}, {x: 23, y: 20}, {x: 28, y: 39}, {x: 31, y: 0}];
+  labels = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
   constructor(private weather: DashboardService) { }
 
   ngOnInit() {
@@ -29,22 +31,21 @@ export class LineChartComponent implements OnInit {
       console.log(weatherDates);
 
       this.chart = new Chart('canvas', {
+        
         type: 'line',
         data: {
-          labels: weatherDates,
+          labels: this.labels,
           datasets: [
-            { 
-              data: temp_max,
+            {
+              data: this.data,
               borderColor: "#3cba9f",
-              fill: 'origin'
-            },
-            { 
-              data: temp_min,
-              borderColor: "#ffcc00",
-              backgroundColor: "#cc65fe",
-              fill: false,
               lineTension: 0
             },
+            {
+              data: this.dataTwo,
+              borderColor: "#ffcc00",
+              lineTension: 0
+            }
           ]
         },
         options: {
@@ -53,24 +54,63 @@ export class LineChartComponent implements OnInit {
           },
           scales: {
             xAxes: [{
-              display: true,
-              gridLines: {
-                color: 'rgba(0, 0, 0, 0.3)',
-              },
-              ticks: {
-              }
+              display: true
             }],
             yAxes: [{
               display: true,
-              gridLines: {
-                color: 'rgba(0, 0, 0, 0.3)'
-              },
               ticks: {
+                min: 0,
+                max: 120,
+                stepSize: 30
               }
-            }],
+            }]
           }
         }
-      });     
+      });
+
+      // this.chart = new Chart('canvas', {
+      //   type: 'line',
+      //   data: {
+      //     labels: weatherDates,
+      //     datasets: [
+      //       { 
+      //         data: temp_max,
+      //         borderColor: "#3cba9f",
+      //         fill: 'origin'
+      //       },
+      //       { 
+      //         data: temp_min,
+      //         borderColor: "#ffcc00",
+      //         backgroundColor: "#cc65fe",
+      //         fill: false,
+      //         lineTension: 0
+      //       },
+      //     ]
+      //   },
+      //   options: {
+      //     legend: {
+      //       display: false
+      //     },
+      //     scales: {
+      //       xAxes: [{
+      //         display: true,
+      //         gridLines: {
+      //           color: 'rgba(0, 0, 0, 0.3)',
+      //         },
+      //         ticks: {
+      //         }
+      //       }],
+      //       yAxes: [{
+      //         display: true,
+      //         gridLines: {
+      //           color: 'rgba(0, 0, 0, 0.3)'
+      //         },
+      //         ticks: {
+      //         }
+      //       }],
+      //     }
+      //   }
+      // });     
     })
   }
 }

56ea50e31c5522bc631460bda850c20c3b34ed95
MrrGoodCat <mrrgoodcat@gmail.com>
2019-01-03 02:28:27 +0200

    Weather forecast is working

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -11,11 +11,10 @@ export class DashboardService {
 
   constructor(private httpClient: HttpClient) { }
 
-  private headers = new HttpHeaders()
-            .set("X-CustomHeader", "custom header value");
+  //private headers = new HttpHeaders({'Access-Control-Allow-Origin': '*', "Access-Control-Allow-Headers": "*","Access-Control-Allow-Methods": "GET, POST, OPTIONS"});
 
   dailyForecast() {
-    return this.httpClient.get("https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b6907d289e10d714a6e88b30761fae22")
-    .pipe(map(result => result, Headers));
+    return this.httpClient.get("https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=a5b50d7e7da8c9c06c7e2fe623ec0f6e")
+    .pipe(map(result => result));
   }
 }
